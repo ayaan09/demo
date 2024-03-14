c4451a6ab8f70dd021d16385ca00afd8985a9b95
ayaan09 <hanzalah.choudhury@gmail.com>
2024-03-14 14:10:07 +0800

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import './buttoncomp'
+import ButtonComponent from './buttoncomp';
 
-function App() {
+const videoFiles = [
+  require('./videos/station1_in.mp4'),
+  require('./videos/Station2_in.mp4'),
+  require('./videos/station3_in.mp4'),
+  require('./videos/station1_out.mp4'),
+  require('./videos/station2_out.mp4'),
+  require('./videos/Station3_out.mp4'),
+];
+
+const VideoPane = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="video-pane">
+    <div className='header1'>OD Matrix Generator</div>
+      <div className="video-row">
+      <div className='textt'>  IN:</div>
+        <div className="video-column">
+      <div className='forh3'> 
+      <h3>STATION 1</h3>
+          <video width="300" height="250" controls>
+            <source src={videoFiles[0]} type="video/mp4" />
+          </video>
+          </div>
+        </div>
+        <div className="video-column">
+
+        <div className='forh3'>
+          <h3> STATION 2</h3>
+          <video width="300" height="250" controls>
+            <source src={videoFiles[1]} type="video/mp4" />
+          </video>
+          </div>
+        </div>
+        <div className="video-column">
+        
+        <div className='forh3'>
+          <h3> STATION 3</h3>
+          <video width="300" height="250" controls>
+            <source src={videoFiles[2]} type="video/mp4" />
+          </video>
+          </div>
+        </div>
+      </div>
+      <div className="video-row"> 
+      <div className='textt'>OUT:</div>
+        <div className="video-column">
+          <video width="300" height="250" controls>
+            <source src={videoFiles[3]} type="video/mp4" />
+          </video>
+        </div>
+        <div className="video-column"> 
+          <video width="300" height="250" controls>
+            <source src={videoFiles[4]} type="video/mp4" />
+          </video>
+        </div>
+        <div className="video-column">
+          <video width="300" height="250" controls>
+            <source src={videoFiles[5]} type="video/mp4" />
+          </video>
+        </div>
+      </div>
+      <ButtonComponent/>
     </div>
   );
-}
+};
 
-export default App;
+export default VideoPane;
